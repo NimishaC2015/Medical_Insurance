d25b0afeb9da80b05a4252d0a635dfaa4673f4a8
unknown <nimikartika33@gmail.com>
2022-10-22 16:11:09 +0530

    config file added

---
+++ config.py
@@ -0,0 +1,6 @@
+import os
+CSV_FILE_PATH='Enter csv file path'
+JSON_FILE_PATH=os.path.join(r'D:\python practice\Practice_velocity\Data Science\Machine Learning\10_11_Flask\practice','data.json')
+MODEL_PATH=os.path.join(r'D:\python practice\Practice_velocity\Data Science\Machine Learning\10_11_Flask\practice','linear_model.pkl')
+
+PORT_NUMBER=5005
